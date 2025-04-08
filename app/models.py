d008f9d8a984ce0e35d67ad43d994d3c0d3be8f1
Jairo Reyes <ja.reyesr1@uniandes.edu.co>
2025-04-08 14:19:53 -0500

    Se modifica modelo para evitar dependencia deprecada

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from . import db
-from datetime import datetime
+from datetime import datetime, timezone
 
 class BlacklistedEmail(db.Model):
     __tablename__ = 'blacklisted_emails'
@@ -9,5 +9,5 @@ class BlacklistedEmail(db.Model):
     app_uuid = db.Column(db.String(36), nullable=False)
     blocked_reason = db.Column(db.String(255), nullable=True)
     ip_address = db.Column(db.String(45), nullable=False)  # IPv6 compatible
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
 
